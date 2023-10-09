ec872432fdbbf8c1e73261480f3db3bbdf4f9919
Abdelmajid El-oualy <abdel-ou@c1r3p4.1337.ma>
2023-10-09 08:52:30 +0100

    -45

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: abdel-ou <abdel-ou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 08:56:31 by abdel-ou          #+#    #+#             */
-/*   Updated: 2023/10/08 22:10:52 by abdel-ou         ###   ########.fr       */
+/*   Updated: 2023/10/09 08:50:53 by abdel-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,16 +77,15 @@ void	drow_player(t_mlx mlxx, int color)
 			drow_rays(&mlxx, angle + 0.2, 0x00ff00);
 			drow_rays(&mlxx, angle + 0.15, 0x00ff00);
 			drow_rays(&mlxx, angle + 0.1, 0x00ff00);
-	
-		
-		
+			
 } 
 int    drow(void *param)
 {
-    int i;
-    int j;
+    int	i;
+    int	j;
+	t_mlx *mlxx;
 
-	t_mlx *mlxx = param;
+	mlxx = (t_mlx *)param;
     i = 0;
     j = 0;
 	while (mlxx->map[i])
