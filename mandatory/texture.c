8213419857d1130d673bfc6a72592e6a5dc082bc
Hassan Aguezoum <haguezou@c1r2p7.1337.ma>
2023-11-15 14:51:34 +0100

    delete unused printf

--- mandatory/texture.c
+++ mandatory/texture.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   texture.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abdel-ou <abdel-ou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: haguezou <haguezou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:52:19 by abdel-ou          #+#    #+#             */
-/*   Updated: 2023/11/14 13:23:08 by abdel-ou         ###   ########.fr       */
+/*   Updated: 2023/11/15 14:49:44 by haguezou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ unsigned int	*load_color(t_mlx *mlx, char *path)
 	new = mlx_xpm_file_to_image(mlx->mlx, path, &height, &weight);
 	if (new == NULL || height != 40 || weight != 40)
 		return (NULL);
-	printf("width =>%d | height =>%d\n", weight, height);
 	img_data = (unsigned int *)mlx_get_data_addr(new,
 			&weight, &endian, &endian);
 	return (img_data);
